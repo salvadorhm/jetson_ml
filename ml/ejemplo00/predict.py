4d8d9a535a77940fab764b12b2aec7bb6e9d48da
Salvador HM <salvadorhm@gmail.com>
2021-09-22 13:42:40 -0500

    ejemplo00 v4

--- ml/ejemplo00/predict.py
+++ ml/ejemplo00/predict.py
@@ -2,7 +2,8 @@ import tensorflow as tf
 from tensorflow import keras
 
 model = tf.keras.models.load_model('model.h5')
-
-x = float(input("X value:"))
-y = model.predict([x])
-print(y)
+while True:
+    x = float(input("X value:"))
+    y = model.predict([x])
+    print(y)
+    print("Press Ctrl + c to finish")
